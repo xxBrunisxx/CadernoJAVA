91ad4df40b6d827c4c6cbdb4f5999bfd60831edd
xxBrunisxx <131306196+xxBrunisxx@users.noreply.github.com>
2023-05-26 15:37:28 -0300

    Add files via upload

---
+++ main.kt
@@ -0,0 +1,20 @@
+fun main() {
+  println("olá mundo!")
+var nome = "brunis"
+val nascimento = 2005
+
+println(nome)          // exibe nome
+println(nascimento)     // exibe nascimento
+
+val myNum: Int = 5                // Int
+val myDoubleNum: Double = 5.99    // Double
+val myLetter: Char = 'D'          // Char
+val myBoolean: Boolean = true     // Boolean
+val myText: String = "Olá"      // String
+
+  println (myNum)
+  println (myDoubleNum)
+  println (myLetter)
+  println (myBoolean)
+  println (myText)
+}
