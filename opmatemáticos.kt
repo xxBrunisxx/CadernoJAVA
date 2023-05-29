f69a7276be80047daef06446cd4b3178c5169667
xxBrunisxx <131306196+xxBrunisxx@users.noreply.github.com>
2023-05-29 17:25:06 -0300

    Create ope.matemáticos.kt

---
+++ opmatemáticos.kt
@@ -0,0 +1,31 @@
+fun main() {
+  //operadores matemáticos
+  var x = 13
+  var y = 3
+  //exibir a soma de x e y
+  var soma=x+y
+  println("A Soma é: "+soma)
+  //exibir a subtração de x e y
+  var sub=x-y
+  println("A subtração é: "+sub)
+  //exibit a multiplicação entre x e y
+  var mult=x*y
+  println("A multiplicação é: "+mult)
+  //exibir a divisão entre x e y
+  var div=x/y
+  println("A divisão é: "+div)
+  // resto da divisao de x por y
+  var rest=x%y
+  println("O resto da divisão é: "+rest)
+
+  //incremento
+  println("O x é: "+ x)
+  ++x
+  println("Agora o x mais 1 é: "+x)
+
+  //decremento
+  --x
+  println("agora o x menos 1 é: "+x)
+
+  
+}
