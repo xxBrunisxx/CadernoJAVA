dd79d8cf914c163ba01424077748406edfe484b2
xxBrunisxx <131306196+xxBrunisxx@users.noreply.github.com>
2023-05-29 17:18:44 -0300

    Create Main.kt

---
+++ Main.kt
@@ -0,0 +1,20 @@
+fun main() {
+  println("olá Mundo")
+  var nome: String = "Edney"
+  val nascimento: Int = 1975
+
+  println(nome)          // exibe o nome
+  println(nascimento)     // exibe o nascimento
+
+  val myNum: Int = 5                // Int
+  val myDoubleNum: Double = 5.99    // Double
+  val myLetter: Char = 'D'          // Char
+  val myBoolean: Boolean = true     // Boolean
+  val myText: String = "Olá"      // String
+
+  println (myNum)
+  println (myDoubleNum)
+  println (myLetter)
+  println (myBoolean)
+  println (myText)
+}
