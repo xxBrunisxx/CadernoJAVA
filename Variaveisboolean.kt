fa956c1d44c89e27caf7b05fd90cf12f5b2b3d96
xxBrunisxx <131306196+xxBrunisxx@users.noreply.github.com>
2023-06-06 17:43:29 -0300

    Create Variaveisboolean.kt

---
+++ Variaveisboolean.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+  //criar uma variavel do tipo booleana (false true)
+  var nomeVariavel : Boolean = false 
+  println(nomeVariavel)
+  var x: Boolean = true 
+  println(x)
+  //if faz teste e retorna boolean 
+
+  if (x){
+    println("verdadeiro")
+  }
+  else{
+    println("falso")
+  }
+}
